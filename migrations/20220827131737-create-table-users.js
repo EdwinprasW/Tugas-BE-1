309fecf65cfd1bf2743e25d8d7dc81ee365fc99f
EdwinprasW <weather2402@gmail.com>
2022-09-09 13:00:03 +0700

    new migration

---
+++ migrations/20220827131737-create-table-users.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const sequelize = require("sequelize");
+
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("users", {
+
+            id: {
+                type: sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false,
+            },
+            email: {
+                type: sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            password: {
+                type: sequelize.STRING,
+                allowNull: false,
+            },
+            name: {
+                type: sequelize.STRING,
+                allowNull: false,
+            },
+            role: {
+                type: Sequelize.ENUM("admin", "user"),
+                allowNull: false,
+            },
+            photo: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+
+
+
+        })
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add reverting commands here.
+         *
+         * Example:
+         * await queryInterface.dropTable('users');
+         */
+    }
+};
